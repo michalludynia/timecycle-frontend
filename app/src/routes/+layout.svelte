c9088236755b3f59310facba9dab13da5b156c2b
Michal Ludynia <michal.ludynia@transfergo.com>
2023-09-24 21:47:45 +0200

    Added tailwind with UI package

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { page } from '$app/stores';
+
+	const tabs = [
+		{ path: '/insert', label: 'Wprowadź' },
+		{ path: '/edit', label: 'Edytuj' },
+		{ path: '/days-off', label: 'Dni wolne' },
+		{ path: '/settings', label: 'Ustawienia' },
+		{ path: '/reports', label: 'Raporty' },
+		{ path: '/my-account', label: 'Moje konto' },
+		{ path: '/logout', label: 'Wyloguj' }
+	];
+
+	$: activeUrl = $page.url.pathname;
+</script>
+
+<Navbar let:hidden let:toggle>
+	<NavBrand href="/">
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+			>Timecycle</span
+		>
+	</NavBrand>
+	<NavHamburger on:click={toggle} />
+	<NavUl {activeUrl} {hidden}>
+		{#each tabs as tab}
+			<NavLi href={tab.path}>{tab.label}</NavLi>
+		{/each}
+	</NavUl>
+</Navbar>
+
+<slot />
---
+++ app/src/routes/insert/+page.svelte
@@ -0,0 +1,67 @@
+<script>
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell,
+		Button,
+		Input,
+		Label
+	} from 'flowbite-svelte';
+
+	let rowsCount = 1;
+
+	function addRow() {
+		rowsCount = rowsCount + 1;
+	}
+</script>
+
+<div class="px-48">
+	<div class="mb-4">
+		<Label for="insert-date" class="block mb-2">Data</Label>
+		<Input id="insert-date" type="date" />
+	</div>
+	<div class="mb-4">
+		<Label for="insert-interval" class="block mb-2">Przedział czasowy</Label>
+		<Input id="insert-interval" type="text" />
+	</div>
+	<div class="mb-4">
+		<Label for="insert-context" class="block mb-2">Kontekst</Label>
+		<Input id="insert-context" type="text" />
+	</div>
+
+	<div class="mt-20">
+		<Table noborder={true}>
+			<TableHead>
+				<TableHeadCell>Element kontekstu</TableHeadCell>
+				<TableHeadCell>Minuty</TableHeadCell>
+				<TableHeadCell>Czynności</TableHeadCell>
+				<TableHeadCell>Komentarz</TableHeadCell>
+			</TableHead>
+			<TableBody>
+				{#each { length: rowsCount } as _, i}
+					<TableBodyRow>
+						<TableBodyCell>
+							<Input id={'insert-subcontext-' + i} type="text" />
+						</TableBodyCell>
+						<TableBodyCell>
+							<Input id={'insert-minutes-' + i} type="text" />
+						</TableBodyCell>
+						<TableBodyCell>
+							<Input id={'insert-activity-' + i} type="text" />
+						</TableBodyCell>
+						<TableBodyCell>
+							<Input id={'insert-comment-' + i} type="text" />
+						</TableBodyCell>
+					</TableBodyRow>
+				{/each}
+			</TableBody>
+		</Table>
+		<div class="flex justify-between">
+			<Button class="ml-4" color="light" on:click={addRow}>Dodaj wiersz</Button>
+			<Button class="mr-4" color="green">Zapisz</Button>
+		</div>
+	</div>
+</div>
